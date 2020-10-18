8e34a033f5aac3e9d878d093eadfd0cbfc7615d4
Alan RL <alan.r.l.4018@gmail.com>
2020-10-18 08:49:43 -0500

    Arreglando peticion

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -8,11 +8,11 @@ const root = 'http://localhost:6969';
 
 export async function generateToken() {
 	try {
-		const r = await axios
-			.post(root + '/api/validation/login', {
+		const r = (
+			await axios.post(root + '/api/validation/login', {
 				publicKey: '7Kb443PWqFBP5iO84pnSYA==',
 			})
-			.then((r) => r.json());
+		).data;
 		console.log(r);
 		cookie.set('token', r.token, { path: '/' });
 	} catch (err) {
@@ -23,13 +23,14 @@ export async function generateToken() {
 
 export async function requestPost(url, body) {
 	try {
-		const r = await axios
-			.post(root + url, body, {
+		const r = (
+			await axios.post(root + url, body, {
 				headers: {
 					authorization: cookie.get('token'),
 				},
 			})
-			.then((r) => r.json());
+		).data;
+
 		return r;
 	} catch (err) {
 		console.log(err.response);
@@ -39,13 +40,13 @@ export async function requestPost(url, body) {
 
 export async function requestFile(formData) {
 	try {
-		const r = await axios
-			.post(root + '/api/file/uploadFile', formData, {
+		const r = (
+			await axios.post(root + '/api/file/uploadFile', formData, {
 				headers: {
 					'Content-type': 'multipart/form-data',
 				},
 			})
-			.then((r) => r.json());
+		).data;
 		return r;
 	} catch (err) {
 		console.log(err.response);
